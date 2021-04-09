efbd3ef8e90bac72a67d1f0e745762a64d29fcf9
Francisco Rodriguez <FRodriguez@bluecedar.com>
2021-04-09 14:29:23 -0500

    include changes on main.tf

--- main.tf
+++ main.tf
@@ -11,27 +11,27 @@ resource "docker_network" "local_network" {
   }
 }
 
-module vault {
+module "vault" {
   source      = "./modules/vault"
   tag_version = "1.0.0"
 }
 
-module postgres {
+module "postgres" {
   source      = "./modules/postgres"
   tag_version = "9"
 }
 
-module statsd-exporter {
-  source      = "./modules/statsd_exporter"
-  tag_version = "v0.18.0"
-}
-
-module prometheus {
-  source      = "./modules/prometheus"
-  tag_version = "v2.21.0"
-}
-
-module grafana {
-  source      = "./modules/grafana"
-  tag_version = "7.2.0-ubuntu"
-}
+#module statsd-exporter {
+#  source      = "./modules/statsd_exporter"
+#  tag_version = "v0.18.0"
+#}
+#
+#module prometheus {
+#  source      = "./modules/prometheus"
+#  tag_version = "v2.21.0"
+#}
+#
+#module grafana {
+#  source      = "./modules/grafana"
+#  tag_version = "7.2.0-ubuntu"
+#}
